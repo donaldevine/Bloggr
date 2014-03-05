75bf269078fa1a651024a180ab7bd7825fae4d89
Donal Devine <donaldevine@gmail.com>
2014-03-05 02:07:55 +0000

    added routing, templates and models

--- js/app.js
+++ js/app.js
@@ -1,2 +1,31 @@
 App = Ember.Application.create();
 
+App.Router.map(function() {
+    this.resource('about');
+    this.resource('posts', function() {
+        this.resource('post', { path: ':post_id' });
+    });
+});
+
+App.PostsRoute = Ember.Route.extend({
+    model: function() {
+        return posts;
+    }
+});
+
+var posts = [{
+    id: '1',
+    title: "Title of post 1",
+    author: { name: "Donal Devine"},
+    date: new Date('12-27-2012'),
+    excerpt: "This is an exceprt...",
+    body: "This is thge body...."
+}, {
+    id: '2',
+    title: "This is post 2",
+    author: { name: "Donal Devine"},
+    date: new Date('12-24-2012'),
+    excerpt: "Excerpt goes like this...",
+    body: "The body of this post goes here...."
+}];
+
